5ef6d92622e7a21304d53be0f783e589cb259f98
baisaarnina1208@gmail.com <baisaarnina1208@gmail.com>
2025-04-08 22:59:18 +0800

    TopBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from '../src/assets/image/logo.png';
 import './App.css';
-import Home from './page/home';
+import TopBar from './component/topbar';
 import { Routes, Route } from 'react-router-dom';
+import Introduksyon from './page/introduksyon';
 
 
 function App() {
   return (
-    <div className='App'>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
+    <div className='App' style={{backgroundColor:'#FDE5D4'}}>
+      <TopBar/>
+      <Routes path='/' element={<Introduksyon/>}/> 
     </div>
   );
 }
